106576ada05d262b943730dc284c5d48a81dcdcf
astider <astider@gmail.com>
2019-09-01 18:34:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Dexie from 'dexie'
 
-function App() {
+import Form from './Form'
+
+const App = () => {
+  const [open, setOpen] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '2rem auto', width: '200px' }}>
+      <button onClick={() => setOpen(!open)}>{`${
+        open ? 'Close' : 'Open'
+      } Form`}</button>
+      {/* Pass in a new connection to the database when Form is first rendered */}
+      {open && <Form db={new Dexie('FormDatabase')} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
